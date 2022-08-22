6f55fed0e5c6f37b2cef36db0ed3411ef34d77cf
sotohans833 <soto.hans833@gmail.com>
2022-08-22 12:10:16 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function startGame(){
+function startGame() {
     let buttonPet = document.getElementById("button-pet");
     buttonPet.addEventListener("click", choosePet);
     let buttonFire = document.getElementById("button-fire");
@@ -6,8 +6,9 @@ function startGame(){
     let buttonEarth = document.getElementById("button-earth");
     let buttonReset = document.getElementById("button-reset");
 }
+window.addEventListener("load", startGame);
 
-function choosePet(){
+function choosePet() {
     let hipodoge = document.getElementById("Hipodoge");
     let Capipepo = document.getElementById("Capipepo");
     let Ratigueya = document.getElementById("Ratigueya");
@@ -16,38 +17,36 @@ function choosePet(){
     let Pydos = document.getElementById("Pydos");
     let ownPet = document.getElementById("own-pet");
     if (hipodoge.checked) {
-        ownPet.innerHTML = "Hipodoge";
+    ownPet.innerHTML = "Hipodoge";
     } else if (Capipepo.checked) {
-        ownPet.innerHTML = "Capipepo";
+    ownPet.innerHTML = "Capipepo";
     } else if (Ratigueya.checked) {
-        ownPet.innerHTML = "Ratigueya";
+    ownPet.innerHTML = "Ratigueya";
     } else if (Langostelvis.checked) {
-        ownPet.innerHTML = "Langostelvis";
+    ownPet.innerHTML = "Langostelvis";
     } else if (Tucapalma.checked) {
-        ownPet.innerHTML = "Pydos";
+    ownPet.innerHTML = "Tucapalma";
     } else if (Pydos.checked) {
-        ownPet.innerHTML = "Hipodoge";
-    }else{
-        alert("No choice, please choose one pet")
-    }
-    function random(min, max){
-        return Math.floor(Math.random() * (max-min+1)+min)
+    ownPet.innerHTML = "Pydos";
+    } else {
+    alert("No choice, please choose one pet");
     }
     let enemy = random(1, 6);
     let enemyPet = document.getElementById("enemy-pet");
     if (enemy == 1) {
-        enemyPet.innerHTML = "Hipodoge";
+    enemyPet.innerHTML = "Hipodoge";
     } else if (enemy == 2) {
-        enemyPet.innerHTML = "Capipepo";
+    enemyPet.innerHTML = "Capipepo";
     } else if (enemy == 3) {
-        enemyPet.innerHTML = "Ratigueya";
+    enemyPet.innerHTML = "Ratigueya";
     } else if (enemy == 4) {
-        enemyPet.innerHTML = "Langostelvis";
+    enemyPet.innerHTML = "Langostelvis";
     } else if (enemy == 5) {
-        enemyPet.innerHTML = "Pydos";
+    enemyPet.innerHTML = "Pydos";
     } else if (enemy == 6) {
-        enemyPet.innerHTML = "Hipodoge";
+    enemyPet.innerHTML = "Hipodoge";
     }
 }
-
-window.addEventListener("load", startGame)
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
